8093ad3ed56c9b4c88eb13f84e31562caddc29ba
angelo <angelo.longano@gmail.com>
2020-03-15 20:32:36 +0100

    API formato json funzionanti (bisogna gestire il CORS)

---
+++ Controllers/SmartTourController.cs
@@ -0,0 +1,23 @@
+﻿using SmartTourAPI.Helpers;
+using SmartTourAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SmartTourAPI.Controllers
+{
+    public class SmartTourController : ApiController
+    {
+        public HttpResponseMessage GetConsumers()
+        {
+            var consumers = DatabaseHelper.GetAllCosumers();
+            return new HttpResponseMessage()
+            {
+                Content = new JsonHelper(consumers)
+            };
+        }
+    }
+}
---
+++ Helpers/DatabaseHelper.cs
@@ -0,0 +1,29 @@
+﻿using Dapper;
+using MySql.Data.MySqlClient;
+using SmartTourAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace SmartTourAPI.Helpers
+{
+    public class DatabaseHelper
+    {
+        private static string ConnectionString =
+            ConfigurationManager.ConnectionStrings["Database"].ConnectionString; //personalizzare la set per gestire l'eccezione
+
+        public static List<Consumer> GetAllCosumers()
+        {
+            var consumers = new List<Consumer>();
+            using (var connection = new MySqlConnection(ConnectionString))
+            {
+                var sql = "select * from consumer";
+                consumers = connection.Query<Consumer>(sql).ToList();
+            }
+            return consumers;
+        }
+
+    }
+}
